4dc489e6634208e59790aecc177a3dd4f773e20a
kctang <kctang@big2.net>
2015-11-01 16:00:13 +0800

    Drop down menuItems when more than two items specified

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # mdl-ui
 
-WARNING: Still figuring out how this should work. Not much to see for now.
+WARNING: Still figuring out how this should work. You can have a look at the work in progress in the `fuse-demo` repository. 
 
 The idea is to create a set of consistent, reusable templates/layouts for Meteor based applications using material design lite.
 
+## TODO
+
+- Create layout for dialogs based on https://www.google.com/design/spec/components/dialogs.html#dialogs-specs
--- layouts/page/page-layout.js
+++ layouts/page/page-layout.js
@@ -1,8 +1,33 @@
+/**
+ * @typedef {object} menuItems
+ * @property {string} label
+ * @property {function|string} action
+ * @property {string} [icon]
+ */
+/**
+ *
+ * @type {object}
+ * @property {menuItems|boolean} [menuItems] Menu items definition. False will not display menu items
+ */
 var defaultOptions = {
     menuItems: false
 };
 
 var validKeys = _.keys(defaultOptions);
+
+function resolveMenuItems(context) {
+    // menu items discovery order:
+    // 1. from template's data context
+    // 2. global MdlUi.options
+    // 3. default options defined by this layout
+    var options = _.clone(defaultOptions);
+    _.extend(options,
+        _.pick(MdlUi.options, validKeys),
+        _.pick(context, validKeys)
+    );
+
+    return options.menuItems;
+}
 
 Template.mdlUiPageLayout.helpers({
     title() {
@@ -10,26 +35,20 @@ Template.mdlUiPageLayout.helpers({
     },
 
     menuItems() {
-        // menu items discovery order:
-        // 1. from template's data context
-        // 2. global MdlUi.options
-        // 3. default options defined by this layout
-        var options = _.clone(defaultOptions);
-        _.extend(options,
-            _.pick(MdlUi.options, validKeys),
-            _.pick(this, validKeys)
-        );
+        return resolveMenuItems(this);
+    },
 
-        return options.menuItems;
+    menuItemsDropDown() {
+        return resolveMenuItems(this).length > 2;
     }
 });
 
 Template.mdlUiPageLayout.events({
-    'click .mdl-layout-icon'() {
+    'click button.back'() {
         history.go(-1);
     },
 
-    'click .mdl-menu__item'(event, template) {
+    'click button.action'(event, template) {
         if (_.isFunction(this.action)) {
             this.action(event, template);
         } else if (_.isString(this.action)) {
