badd5f78bdab0e2070ded77e480d2faebf239364
kctang <kctang@big2.net>
2015-11-08 06:05:29 +0800

    Publish package & bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'kctang:mdl-ui',
-    version: '0.0.4',
+    version: '0.0.5',
     summary: 'Reusable application templates/layouts (experimental)',
     git: 'https://github.com/kctang/mdl-ui',
     documentation: 'README.md'
