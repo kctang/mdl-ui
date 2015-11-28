d6f2b09d6b9dc8f6fe61073b822fb39e4cee4eb1
kctang <kctang@big2.net>
2015-11-28 08:15:43 +0800

    Add search button

--- layouts/app/app-layout.js
+++ layouts/app/app-layout.js
@@ -1,34 +1,53 @@
 var defaultOptions = {
-    publicDrawer: 'mdlUiAppDrawerPublic',
-    drawer: 'mdlUiAppDrawer'
+  publicDrawer: 'mdlUiAppDrawerPublic',
+  drawer: 'mdlUiAppDrawer',
+
+  search: false,
+  searchSessionName: 'search'
+
 };
 
 var options = _.clone(defaultOptions);
 
 Template.mdlUiAppLayout.helpers({
-    title() {
-        var o = MdlUi.options;
-        return _.isFunction(o.title) ? o.title() : o.title;
-    },
+  title() {
+    var o = MdlUi.options;
+    return _.isFunction(o.title) ? o.title() : o.title;
+  },
 
-    menuItems() {
-        return MdlUi.options.menuItems;
-    },
+  menuItems() {
+    return MdlUi.options.menuItems;
+  },
 
-    drawerTemplate() {
-        // populate options with customization from MdlUi.options
-        _.extend(options, _.pick(MdlUi.options, _.keys(defaultOptions)));
+  search() {
+    // TODO: proper clone in template instance
+    var options = _.extend({}, _.pick(MdlUi.options, _.keys(defaultOptions)));
+    return options.search;
+  },
 
-        if(Meteor.user()) {
-            return options.drawer;
-        } else {
-            return options.publicDrawer;
-        }
+  drawerTemplate() {
+    // populate options with customization from MdlUi.options
+    _.extend(options, _.pick(MdlUi.options, _.keys(defaultOptions)));
+
+    if (Meteor.user()) {
+      return options.drawer;
+    } else {
+      return options.publicDrawer;
     }
+  },
+
+  searchValue() {
+    return Session.get(options.searchSessionName);
+  }
 });
 
 Template.mdlUiAppLayout.events({
-    'click .mdl-menu__item': function () {
-        Router.go(this.action);
-    }
+  'click .mdl-menu__item'() {
+    Router.go(this.action);
+  },
+
+  'keydown .search .mdl-textfield__input': _.throttle(function (e) {
+    Session.set(options.searchSessionName, $(e.target).val());
+  }, 500)
+
 });
