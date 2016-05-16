94157a0e7237f084757c24348c150e80988c6d24
kctang <kctang@big2.net>
2016-05-16 21:51:44 +0300

    Remove Meteor package (transition to React based npm package)

--- demos/settings/settings-demo.js
+++
@@ -1,3 +0,0 @@
-Template.mdlUiSettingsDemo.helpers({
-
-});
--- layouts/app/app-drawer-public.js
+++
@@ -1,9 +0,0 @@
-
-Template.mdlUiAppDrawerPublic.events({
-    'click button.login': function (e) {
-        var $drawer = $(e.target).parent().parent().parent();
-        $drawer.removeClass('is-visible');
-        $drawer.parent().find('.mdl-layout__obfuscator').removeClass('is-visible');
-        Router.go('/sign-in');
-    }
-});
